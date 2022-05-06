7c26c536a4e920eb307cd222abcc425295defc25
AJ-Kulundu <jameskulundu@gmail.com>
2022-05-06 12:52:08 +0300

    Editing the header details

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+
+function Header() {
+  return (
+    <Head>
+      <title>Crypto Info</title>
+      <meta name="description" content="Website to display info on cryptocurrencies" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
+export default Header;
