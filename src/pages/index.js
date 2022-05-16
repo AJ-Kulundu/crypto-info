746a2dfbdb565ed33330c89ced52228135d8269d
AJ-Kulundu <jameskulundu@gmail.com>
2022-05-16 22:45:22 +0300

    Adding Millify to better display data on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,56 +7,91 @@ import {
   Avatar,
   HStack,
   Stack,
-  Flex
+  Flex,
 } from "@chakra-ui/react";
 import { useHomeQuery } from "../services/api";
+import millify from "millify";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const { data, isLoading, isSuccess, error } = useHomeQuery();
+  const router = useRouter();
   const StatData = data;
   console.log(StatData);
   return (
-    <Flex p={4} justify="flex-start" align="center" >
+    <Flex p={4} justify="center" align="center">
       {isLoading && <Heading>Loading</Heading>}
       {error && <Heading>Error</Heading>}
       {isSuccess && (
         <Box>
           <VStack spacing={"40px"}>
             <Heading>Welcome To Crypto-Info</Heading>
-            <Heading>Bicoin Dominance {StatData.data.btcDominance}</Heading>
+            <Heading>Bitcoin Dominance {StatData.data.btcDominance}</Heading>
             <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10}>
               <Box>
                 <Text>Total Cryptocurrencies</Text>
-                <Text>{StatData.data.totalCoins}</Text>
+                <Text>
+                  {millify(StatData.data.totalCoins, { precision: 3 })}
+                </Text>
               </Box>
               <Box>
                 <Text>Total Exchanges</Text>
-                <Text>{StatData.data.totalExchanges}</Text>
+                <Text>
+                  {millify(StatData.data.totalExchanges, { precision: 3 })}
+                </Text>
               </Box>
               <Box>
                 <Text>Total Markets</Text>
-                <Text>{StatData.data.totalMarkets}</Text>
+                <Text>
+                  {millify(StatData.data.totalMarkets, { precision: 3 })}
+                </Text>
               </Box>
             </SimpleGrid>
             <Heading> Trending Coins</Heading>
-            <Stack spacing="50px"  direction={{base:"column",md:"row"}}>
+            <Stack spacing="50px" direction={{ base: "column", md: "row" }}>
               {StatData.data.bestCoins.map((coin, id) => (
-                <Box border={"1px solid"} key={id} height="150px" width="150px" p={4} borderRadius={"lg"}>
+                <Box
+                  as="a"
+                  border={"1px solid"}
+                  key={id}
+                  height="150px"
+                  width="150px"
+                  p={4}
+                  borderRadius={"lg"}
+                  onClick={() => {
+                    router.push(`coins/${coin.uuid}`);
+                  }}
+                >
                   <HStack>
+                    <Text>
+                      {id + 1}. {coin.symbol}
+                    </Text>
                     <Avatar size="sm" name={coin.name} src={coin.iconUrl} />
-                    <Text>{coin.symbol}</Text>
                   </HStack>
                   <Text>{coin.name}</Text>
                 </Box>
               ))}
             </Stack>
             <Heading>Newest Coins</Heading>
-            <Stack spacing="50px"  direction={{base:"column",md:"row"}}>
+            <Stack spacing="50px" direction={{ base: "column", md: "row" }}>
               {StatData.data.newestCoins.map((coin, id) => (
-                <Box border={"1px solid"} key={id} height="150px" width="150px" p={4} borderRadius={"lg"}>
+                <Box
+                  as="a"
+                  border={"1px solid"}
+                  key={id}
+                  height="150px"
+                  width="150px"
+                  p={4}
+                  borderRadius={"lg"}
+                  onClick={() => {
+                    router.push(`coins/${coin.uuid}`);
+                  }}
+                >
                   <HStack>
-                  <Avatar size="sm" name={coin.name} src={coin.iconUrl} />
-                    <Text>{coin.symbol}</Text>
+                    <Text>
+                      {id + 1}. {coin.symbol}
+                    </Text>
+                    <Avatar size="sm" name={coin.name} src={coin.iconUrl} />
                   </HStack>
                   <Text>{coin.name}</Text>
                 </Box>
