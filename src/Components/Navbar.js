954c792e3afb50d9ff17d60ce0382c5f53bfa7a6
AJ-Kulundu <jameskulundu@gmail.com>
2022-05-21 15:52:45 +0300

    Adding Links to the navigation bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,8 +11,13 @@ import {
   useColorModeValue,
   useDisclosure,
   VStack,
+  Link,
 } from "@chakra-ui/react";
 import { FaSun, FaMoon, FaTimes, FaStream } from "react-icons/fa";
+import { motion } from "framer-motion";
+import NextLink from "next/link";
+
+const MLink = motion(Link);
 
 const Navbar = () => {
   const { isOpen, onToggle } = useDisclosure();
@@ -41,15 +46,15 @@ const Navbar = () => {
       <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
         <Box bg={bgValue} p={4} shadow="md">
           <VStack spacing="25px" pb={10}>
-            <Text>Home</Text>
-            <Text>Coins</Text>
+            <NavItem to="/">Home</NavItem>
+            <NavItem to="/coins">Coins</NavItem>
           </VStack>
         </Box>
       </Slide>
       <Box display={{ base: "none", md: "block" }}>
         <HStack spacing={6}>
-          <Text>Home</Text>
-          <Text>Coins</Text>
+          <NavItem to="/">Home</NavItem>
+          <NavItem to="/coins">Coins</NavItem>
           <Toggle />
         </HStack>
       </Box>
@@ -67,6 +72,16 @@ const Toggle = () => {
       icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
       onClick={() => toggleColorMode()}
     />
+  );
+};
+
+const NavItem = ({ children, to = "/" }) => {
+  return (
+    <NextLink href={to} passHref>
+      <MLink whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+        {children}
+      </MLink>
+    </NextLink>
   );
 };
 
