1d35b22c1d3471227dd24a5060a2d426cf0e2e81
AJ-Kulundu <jameskulundu@gmail.com>
2022-06-01 16:00:20 +0300

    Complete api calls from rtk query

--- src/services/api.js
+++ src/services/api.js
@@ -1,44 +1,41 @@
-import { createApi,fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-const coinRankApiHeaders = {
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': API_KEY
-}
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const coinRankApi = createApi({
-    reducerPath: 'coinRankApi',
-    baseQuery: fetchBaseQuery({baseUrl:"https://coinranking1.p.rapidapi.com"}),
-    endpoints: (builder) => ({
-        home: builder.query({
-            query: () => ({
-                url:`/stats`,
-                method:'GET',
-                headers: coinRankApiHeaders
-            })
-        }),
-        coins: builder.query({
-            query: () => ({
-                url:`/coins`,
-                method:'GET',
-                headers: coinRankApiHeaders
-            })
-        }),
-        coin: builder.query({
-            query: (id) => ({
-                url:`/coin/${id}`,
-                method:'GET',
-                headers:coinRankApiHeaders
-            })
-        }),
-        history: builder.query({
-            query: (id) => ({
-                url:`/coin/${id}/history`,
-                method:'GET',
-                headers:coinRankApiHeaders
-            })
-        }),
-    })
-})
+  reducerPath: "coinRankApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://coinranking1.p.rapidapi.com" }),
+  endpoints: (builder) => ({
+    home: builder.query({
+      query: ({ headers }) => ({
+        url: `/stats`,
+        method: "GET",
+        headers: headers,
+      }),
+    }),
+    coins: builder.query({
+      query: ({ headers }) => ({
+        url: `/coins`,
+        method: "GET",
+        headers: headers,
+        params: { limit: 100 },
+      }),
+    }),
+    coin: builder.query({
+      query: ({ uuid, headers }) => ({
+        url: `/coin/${uuid}`,
+        method: "GET",
+        headers: headers,
+      }),
+    }),
+    history: builder.query({
+      query: ({ id, timePeriod, headers }) => ({
+        url: `/coin/${id}/history`,
+        method: "GET",
+        headers: headers,
+        params: { timePeriod: timePeriod },
+      }),
+    }),
+  }),
+});
 
-
-export const {useHomeQuery, useCoinsQuery, useCoinQuery,useHistoryQuery} = coinRankApi;
+export const { useHomeQuery, useCoinsQuery, useCoinQuery, useHistoryQuery } =
+  coinRankApi;
